71bd32b09bbc6d89f927558f9200173b13c51459
Mann <68820639+Mann-tech13@users.noreply.github.com>
2021-07-17 10:39:15 +0530

    Create Java End-of--file.java

---
+++ Java/Introduction/Java End-of--file.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int i=1;
+        Scanner sc= new Scanner(System.in);
+        while(sc.hasNext()){
+            System.out.println(i+" "+sc.nextLine());
+            i++;
+        }
+        sc.close();
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
