5a472e39a8130bc3cea420701be07ede53ee5e87
Mann <68820639+Mann-tech13@users.noreply.github.com>
2022-01-06 23:17:47 +0530

    Time: 7 ms (21.38%), Space: 49.1 MB (19.29%) - LeetHub

---
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
@@ -0,0 +1,25 @@
+class Solution {
+    
+    public boolean helper(TreeNode node, int k, HashSet<Integer> set) {
+        if(node == null) {
+            return false;       
+        }
+        if(set.contains(k - node.val)) {
+            return true;
+        }
+        else {
+            set.add(node.val);
+        }
+        
+        boolean lans = helper(node.left, k, set);
+        if(lans) return true;
+        boolean rans = helper(node.right, k, set);
+        if(rans) return true;
+        
+        return false;
+    }
+    public boolean findTarget(TreeNode root, int k) {
+        HashSet<Integer> set = new HashSet<>();
+        return helper(root, k, set);
+    }
+}
