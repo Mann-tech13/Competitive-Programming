7cbec278cfb6ae7038e38f193d09d13b9099b524
Mann <68820639+Mann-tech13@users.noreply.github.com>
2022-01-28 12:26:24 +0530

    Time: 7 ms (50.96%), Space: 45.1 MB (8.37%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.java
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -1,35 +1,30 @@
 class Solution {
-    static int romanToInt(String s) {
-        int num = 0;
-        int change = Integer.MAX_VALUE;
-        for(int i=0;i<s.length();i++){
-            if(change>=getNum(s.charAt(i))){
-                num = num+getNum(s.charAt(i));
-            }else{
-                num = num+getNum(s.charAt(i))-2*change;
-            }
-            change = getNum(s.charAt(i));
-        }
-        return num;
-    }
-    static int getNum(char c){
+    public  int romanToInt(String s) {
+		
+		int sum = 0;
+		
+		for(int i=0;i<s.length()-1;i++) {
+			sum = sum + getValue(s.charAt(i));
+			
+			if(getValue(s.charAt(i)) < getValue(s.charAt(i+1))) {
+				sum = sum - getValue(s.charAt(i))*2;
+			}
+			
+		}
+		sum = sum + getValue(s.charAt(s.length()-1));
+		return sum;
+	}
+	
+    private  int getValue(char c){
         switch(c){
-            case 'I':
-                return 1;
-            case 'V':
-                return 5;
-            case 'X':
-                return 10;
-            case 'L':
-                return 50;
-            case 'C':
-                return 100;
-            case 'D':
-                return 500;
-            case 'M':
-                return 1000;
-            default:
-                return -1;
+            case 'I' : return 1;
+            case 'V' : return 5;
+            case 'X' : return 10;
+            case 'L' : return 50;
+            case 'C' : return 100;
+            case 'D' : return 500;
+            case 'M' : return 1000;
+            default : return 0;
         }
     }
 }
