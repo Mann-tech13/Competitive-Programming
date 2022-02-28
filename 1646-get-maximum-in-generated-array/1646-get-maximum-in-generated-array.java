b4f9f8eb568da2069a570f557d8f7d0e81b8fcad
Mann <68820639+Mann-tech13@users.noreply.github.com>
2022-02-28 11:47:16 +0530

    Time: 1 ms (61.60%), Space: 40.8 MB (31.51%) - LeetHub

---
+++ 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int getMaximumGenerated(int n) {
+        if(n<2){
+            return n;
+        }
+        int[] nums = new int[n+1];
+        nums[0] = 0;
+        nums[1] = 1;
+        int max = 1;
+        for(int i=2; i<n+1; i++){
+            nums[i] = i%2 == 0 ? nums[i/2] : nums[i/2] + nums[(i/2)+1];
+            max = Math.max(max, nums[i]);
+        }
+        // Arrays.sort(nums);
+        return max;
+    }
+}
